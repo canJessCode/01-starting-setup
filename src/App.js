b9e4912c8fae92998e51c7eab4939b0ebd196611
Jessica <jessicagough18@gmail.com>
2023-01-23 13:39:27 +0200

    useCallback edit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
 import Button from "./components/UI/Button/Button";
 import "./App.css";
 import DemoOutput from "./components/Demo/DemoOutput";
@@ -7,9 +7,9 @@ function App() {
   const [showParagraph, setShowParagraph] = useState(false);
   console.log("app running");
 
-  const toggleParagraphHandler = () => {
+  const toggleParagraphHandler = useCallback(() => {
     setShowParagraph((prevShowParagraph) => !prevShowParagraph);
-  };
+  }, []);
 
   return (
     <div className="app">
