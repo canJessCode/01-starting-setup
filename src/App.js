2570d25f35586c08ddf9c5f41ca918ba39b9aa9e
Jessica <jessicagough18@gmail.com>
2023-01-23 13:26:12 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React from 'react';
-
-import './App.css';
+import React, { useState } from "react";
+import Button from "./components/UI/Button/Button";
+import "./App.css";
+import DemoOutput from "./components/Demo/DemoOutput";
 
 function App() {
+  const [showParagraph, setShowParagraph] = useState(false);
+  console.log("app running");
+
+  const toggleParagraphHandler = () => {
+    setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+  };
+
   return (
     <div className="app">
       <h1>Hi there!</h1>
+      <DemoOutput show={showParagraph} />
+      <Button onClick={toggleParagraphHandler}>Toggle paragraph</Button>
     </div>
   );
 }
