d203bf6be92372822134b19f555dd26dc7c67360
Jessica <jessicagough18@gmail.com>
2023-01-23 13:44:29 +0200

    button component wrapper

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -4,14 +4,14 @@ import classes from "./Button.module.css";
 
 const Button = (props) => {
   return (
-    <button
+    <Button
       type={props.type || "button"}
       className={`${classes.button} ${props.className}`}
       onClick={props.onClick}
       disabled={props.disabled}
     >
       {props.children}
-    </button>
+    </Button>
   );
 };
 
